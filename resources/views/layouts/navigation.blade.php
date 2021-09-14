e89a9dab57c76e367bba5414245cfe6ae76440af
kvng-shaka <funsho.adeosun01@gmail.com>
2021-09-14 23:31:55 +0100

    .

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,14 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                @if (Auth::user()->admin == true)
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-nav-link :href="route('admin.users.index')">
+                            {{ __('Manage Users') }}
+                        </x-nav-link>
+                    </div>
+                @endif
             </div>
 
             <!-- Settings Dropdown -->
